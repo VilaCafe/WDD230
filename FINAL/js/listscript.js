65a5affb0372d887c03b1bf075a268bac8c92f06
VilaCafe <victorlcferreira@gmail.com>
2022-12-05 21:44:02 -0300

    list ready

---
+++ FINAL/js/listscript.js
@@ -0,0 +1,35 @@
+let dropdown = document.getElementById('select','select2');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option','option2');
+defaultOption.text = 'Choose Fruit';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'https://raw.githubusercontent.com/VilaCafe/WDD230/main/FINAL/data/fruit.js';
+
+const request = new XMLHttpRequest();
+request.open('GET', url, true);
+
+request.onload = function() {
+  if (request.status === 200) {
+    const data = JSON.parse(request.responseText);
+    let option;
+    for (let i = 0; i < data.length; i++) {
+      option = document.createElement('option','option2');
+      option.text = data[i].name;
+      
+      dropdown.add(option);
+    }
+   } else {
+    // Reached the server, but it returned an error
+  }   
+}
+
+request.onerror = function() {
+  console.error('An error occurred fetching the JSON from ' + url);
+};
+
+request.send();
+
